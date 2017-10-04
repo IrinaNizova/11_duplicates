97e4cbdf51bb1620573164c3b7e7aca65d965a6e
Ira <you@example.com>
2017-10-05 02:07:16 +0300

    create task

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Anti-Duplicator
+Script that takes an input folder, looks through all the files in it (and all subfolders and sub-folders ...)
+and reports if it finds duplicates.
 
-[TODO. There will be project description]
+# How to run
+
+python duplicates.py [folder]
+Example: python3 duplicates.py /var/log
+
+# Example of output
+This files have duplicates:
+test/my/java_error_in_PYCHARM_2742.log
+test/my/less/java_error_in_PYCHARM_2742.log
 
 # Project Goals
 
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,29 @@
+import os
+import argparse
+
+
+def find_duplicates(root_dir_path):
+    files_in_folder = []
+    duplicates = []
+    for root, dir, files in os.walk(root_dir_path):
+        for file in files:
+            file_path = "/".join([root, file])
+            if os.path.islink(file_path):
+                continue
+            if (file, os.stat(file_path).st_size) in files_in_folder:
+                duplicates.append(file_path)
+            else:
+                files_in_folder.append((file, os.stat(file_path).st_size))
+    return duplicates
+
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dir_path", help="write name of json file")
+    duplicates = find_duplicates(parser.parse_args().dir_path)
+    if duplicates:
+        print("This files have duplicates: ")
+        for duplicate in duplicates:
+            print(duplicate)
+    else:
+        print("There are no duplicates in this folder")
