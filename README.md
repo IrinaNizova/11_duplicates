65cb39258f7920984f037de6705b4e474aa07985
Ira <you@example.com>
2017-10-21 21:49:45 +0300

    fix bugs

--- README.md
+++ README.md
@@ -3,14 +3,20 @@ Script that takes an input folder, looks through all the files in it (and all su
 and reports if it finds duplicates.
 
 # How to run
-
+```python
 python duplicates.py [folder]
-Example: python3 duplicates.py /var/log
+```
+Example:
+```python
+python3 duplicates.py /var/log
+```
 
 # Example of output
+```
 This files have duplicates:
 test/my/java_error_in_PYCHARM_2742.log
 test/my/less/java_error_in_PYCHARM_2742.log
+```
 
 # Project Goals
 
--- duplicates.py
+++ duplicates.py
@@ -5,15 +5,18 @@ import argparse
 def find_duplicates(root_dir_path):
     files_in_folder = []
     duplicates = []
-    for root, dir, files in os.walk(root_dir_path):
-        for file in files:
-            file_path = "/".join([root, file])
+    for root, dir, file_names in os.walk(root_dir_path):
+        for file_name in file_names:
+            file_path = os.path.join(root, file_name)
             if os.path.islink(file_path):
                 continue
-            if (file, os.stat(file_path).st_size) in files_in_folder:
-                duplicates.append(file_path)
+            file_size = os.path.getsize(file_path)
+            if (file_name, file_size) in files_in_folder:
+                duplicates.append((file_path, file_name))
             else:
-                files_in_folder.append((file, os.stat(file_path).st_size))
+                file_size = os.path.getsize(file_path)
+                files_in_folder.append((file_name, file_size))
+    duplicates.sort(key=lambda x: x[1])
     return duplicates
 
 
@@ -23,7 +26,7 @@ if __name__ == '__main__':
     duplicates = find_duplicates(parser.parse_args().dir_path)
     if duplicates:
         print("This files have duplicates: ")
-        for duplicate in duplicates:
-            print(duplicate)
+        for duplicate_name, _ in duplicates:
+            print(duplicate_name)
     else:
         print("There are no duplicates in this folder")
